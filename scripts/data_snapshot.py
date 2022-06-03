f27ba5a3def30455b43677d533f1ba7ce6f57301
sgbaird <sterling.baird@icloud.com>
2022-06-03 13:47:28 -0600

    dummy and full runs

--- scripts/data_snapshot.py
+++ scripts/data_snapshot.py
@@ -10,18 +10,24 @@ from mp_time_split.utils.data import DUMMY_SNAPSHOT_NAME, _get_data_home
 jsonpickle_pandas.register_handlers()
 # %% dummy data
 mpt = MPTimeSplit(nsites=(1, 2), elements=["V"])
+dummy_expt_df = mpt.fetch_data()
+dummy_data_path = path.join(_get_data_home(), DUMMY_SNAPSHOT_NAME)
+
+with zopen(dummy_data_path, "w") as f:
+    f.write(jsonpickle.encode(dummy_expt_df))
+
+# %% full data
+mpt = MPTimeSplit(nsites=(1, 52))
 expt_df = mpt.fetch_data()
 data_path = path.join(_get_data_home(), DUMMY_SNAPSHOT_NAME)
+with zopen(dummy_data_path, "w") as f:
+    f.write(jsonpickle.encode(dummy_expt_df))
+1 + 1
+
+
+# %% Code Graveyard
 # expt_df.to_json()
 # store_dataframe_as_json(expt_df, data_path, compression="gz")
-
-with zopen(data_path, "w") as f:
-    f.write(jsonpickle.encode(expt_df))
-
 # with zopen(data_path, "wb") as f:
 #     data = json.dumps(expt_df.to_json()).encode()
 #     f.write(data)
-
-# %% full data
-# mpt = MPTimeSplit(nsites=(1, 52))
-1 + 1
