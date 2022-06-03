26d45e7b3f1c43b1ce1642d89b739bf213373f1b
sgbaird <sterling.baird@icloud.com>
2022-06-02 21:51:06 -0600

    test_get_test_data

--- tests/test_mp_time_split.py
+++ tests/test_mp_time_split.py
@@ -19,6 +19,14 @@ def test_get_train_and_val_data():
     return train_inputs, val_inputs, train_outputs, val_outputs
 
 
+def test_get_test_data():
+    mpt = MPTimeSplit(nsites=(1, 2), elements=["V"])
+    mpt.fetch_data()
+    train_input, test_input, train_output, test_output = mpt.get_test_data()
+    return train_input, test_input, train_output, test_output
+
+
 if __name__ == "__main__":
     train_inputs, val_inputs, train_outputs, val_outputs = test_get_train_and_val_data()
+    train_input, test_input, train_output, test_output = test_get_test_data()
     1 + 1
