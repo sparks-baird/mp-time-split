2d8b58ea85d90957a13d76b35563352c22c1cc74
sgbaird <sterling.baird@icloud.com>
2022-06-04 11:50:27 -0600

    add one_by_one test

--- tests/test_mp_time_split.py
+++ tests/test_mp_time_split.py
@@ -7,6 +7,9 @@ from mp_time_split.utils.data import DUMMY_SNAPSHOT_NAME, get_data_home
 
 dummy_data_path = path.join(get_data_home(), DUMMY_SNAPSHOT_NAME)
 
+num_sites = (1, 2)
+elements = ["V"]
+
 
 def test_data_snapshot():
     dummy_expt_df_check = load_dataframe_from_json(dummy_data_path)
@@ -19,8 +22,15 @@ def test_data_snapshot():
         )
 
 
-num_sites = (1, 2)
-elements = ["V"]
+def test_data_snapshot_one_by_one():
+    dummy_expt_df_check = load_dataframe_from_json(dummy_data_path)
+    mpt = MPTimeSplit(num_sites=(1, 2), elements=["V"])
+    dummy_expt_df = mpt.fetch_data(one_by_one=True)
+    dummy_match = dummy_expt_df.compare(dummy_expt_df_check)
+    if not dummy_match.empty:
+        raise ValueError(
+            f"dummy_expt_df and dummy_expt_df_check unmatched: {dummy_match}"
+        )
 
 
 def test_get_train_and_val_data():
@@ -55,8 +65,9 @@ def test_load():
 
 
 if __name__ == "__main__":
-    data = test_load()
     test_data_snapshot()
+    test_data_snapshot_one_by_one()
+    data = test_load()
     train_inputs, val_inputs, train_outputs, val_outputs = test_get_train_and_val_data()
     train_inputs, test_inputs, train_outputs, test_outputs = test_get_test_data()
     data
